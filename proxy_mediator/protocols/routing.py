e8ae7b90e16eec1e0b14435f0d1bc08f12402088
Daniel Bluhm <dbluhm@pm.me>
2021-10-07 15:52:33 -0400

    feat: add routing protocol module with forward handler
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

---
+++ proxy_mediator/protocols/routing.py
@@ -0,0 +1,77 @@
+"""Routing protocol."""
+from typing import Any, Dict
+
+from aries_staticagent.message import BaseMessage
+from aries_staticagent.module import Module, ModuleRouter
+
+from .. import (
+    message_as,
+    agent_connection as agent_connection_var,
+    mediator_connection as mediator_connection_var,
+)
+from ..connections import Connection
+from ..error import Reportable
+
+
+class ForwardError(Reportable):
+    """Base Forward Errors."""
+
+
+class AgentConnectionNotEstablished(ForwardError):
+    """Raised when an agent connection is not established."""
+
+    code = "agent-connection-not-established"
+
+
+class MediatorConnectionNotEstablished(ForwardError):
+    """Raised when no mediator connection is established.
+
+    We should not be receiving forward messages if we haven't connected with the
+    external mediator.
+    """
+
+    code = "mediator-connection-not-established"
+
+
+class ForwardFromUnauthorizedConnection(ForwardError):
+    """Raised when connection forward was received from is not the external mediator."""
+
+    code = "forward-from-unauthorized-connection"
+
+
+class Forward(BaseMessage):
+    to: str
+    msg: Dict[str, Any]
+
+
+class Routing(Module):
+    doc_uri = "did:sov:BzCbsNYhMrjHiqZDTUASHg;spec/"
+    protocol = "routing"
+    version = "1.0"
+    route = ModuleRouter()
+
+    @route
+    @message_as(Forward)
+    async def forward(self, msg: Forward, conn: Connection):
+        """Handle forward message."""
+        agent_connection = agent_connection_var.get(default=None)
+        if not agent_connection:
+            raise AgentConnectionNotEstablished(
+                "Connection to the agent has not yet been established."
+            )
+
+        mediator_connection = mediator_connection_var.get(default=None)
+        if not mediator_connection:
+            raise MediatorConnectionNotEstablished(
+                "Connection to mediator has not yet been established; "
+                "forward messages may only be received from mediator connection"
+            )
+        if conn != mediator_connection:
+            raise ForwardFromUnauthorizedConnection(
+                "Forward messages may only be received from mediator connection"
+            )
+
+        # Assume forward is for the agent connection and just send.
+        # Do not perform any wrapping on message (send as "plaintext") because
+        # message is already packed.
+        await agent_connection.send_async(msg.msg, plaintext=True)
