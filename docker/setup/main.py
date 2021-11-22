b5565cc76abf9f82b5dda4eb6a3f8de5c32fc68f
Char Howland <char@indicio.tech>
2021-11-22 11:05:27 -0700

    fix: add polling to get_proxy_invite()
    Signed-off-by: Char Howland <char@indicio.tech>

--- docker/setup/main.py
+++ docker/setup/main.py
@@ -74,6 +74,10 @@ async def get_proxy_invite() -> dict:
     async with AsyncClient() as client:
         r = await client.get(f"{PROXY}/retrieve_agent_invitation")
         url = r.json()["invitation_url"]
+        while not url:
+            await asyncio.sleep(1)
+            r = await client.get(f"{PROXY}/retrieve_agent_invitation")
+            url = r.json()["invitation_url"]
         return json.loads(urlsafe_b64decode(url.split("c_i=")[1]))
 
 
