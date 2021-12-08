2eacdf3bcdf84bc08c2af764bd49cab67f17d14e
Daniel Bluhm <dbluhm@pm.me>
2021-12-08 15:53:31 -0500

    feat: ping if state reaches only response
    Signed-off-by: Daniel Bluhm <dbluhm@pm.me>

--- docker/setup/main.py
+++ docker/setup/main.py
@@ -13,20 +13,22 @@ from base64 import urlsafe_b64decode
 import json
 from os import getenv
 
-from acapy_client import Client
 from acapy_client.api.connection import (
+    create_invitation,
     get_connection,
     receive_invitation,
-    create_invitation,
 )
 from acapy_client.api.mediation import (
     get_mediation_requests_mediation_id,
     post_mediation_request_conn_id,
     put_mediation_mediation_id_default_mediator,
 )
+from acapy_client.api.trustping import post_connections_conn_id_send_ping
+from acapy_client.client import Client
 from acapy_client.models.conn_record import ConnRecord
 from acapy_client.models.create_invitation_request import CreateInvitationRequest
 from acapy_client.models.mediation_create_request import MediationCreateRequest
+from acapy_client.models.ping_request import PingRequest
 from acapy_client.models.receive_invitation_request import ReceiveInvitationRequest
 from acapy_client.types import Unset
 from httpx import AsyncClient
@@ -77,16 +79,36 @@ async def agent_receive_invitation(agent: Client, invite: dict) -> ConnRecord:
     conn_record = await receive_invitation.asyncio(
         client=agent, json_body=ReceiveInvitationRequest.from_dict(invite)
     )
+
+    conn_states_order = {"invite": 0, "request": 1, "response": 2, "active": 3}
+
+    async def _connection_state(conn_record: ConnRecord, state: str):
+        count = 0
+        assert isinstance(conn_record.state, str)
+        while (
+            conn_states_order[conn_record.state] <= conn_states_order[state]
+            and count < 10
+        ):
+            count += 1
+            await asyncio.sleep(3)
+            assert isinstance(conn_record.connection_id, str)
+            retrieved = await get_connection.asyncio(
+                conn_record.connection_id, client=agent
+            )
+            assert retrieved
+            conn_record = retrieved
+            assert isinstance(conn_record.state, str)
+
     if not conn_record:
         raise RuntimeError("Failed to receive invitation on agent")
 
-    while conn_record.state != "active":
-        await asyncio.sleep(1)
+    await _connection_state(conn_record, "response")
+    if conn_record.state == "response":
         assert isinstance(conn_record.connection_id, str)
-        conn_record = await get_connection.asyncio(
-            conn_record.connection_id, client=agent
+        await post_connections_conn_id_send_ping.asyncio(
+            client=agent, conn_id=conn_record.connection_id, json_body=PingRequest()
         )
-        assert conn_record
+        await _connection_state(conn_record, "active")
 
     return conn_record
 
